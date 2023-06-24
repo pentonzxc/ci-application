69e9b054d11024b5a27def9cc2fecb71325b5791
Nikolai Urusov <nikolai.urusov@gmail.com>
2023-06-24 13:31:19 +0300

    add BagController.java

--- spring-application/src/main/java/com/example/application/BagController.java
+++ spring-application/src/main/java/com/example/application/BagController.java
@@ -2,10 +2,10 @@ package com.example.application;
 
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.parsing.Problem;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -13,30 +13,50 @@ import static com.example.application.BagData.toEntity;
 
 
 @RestController
+@RequestMapping("/users")
 @RequiredArgsConstructor
 public class BagController {
 
     final BagService bagService;
 
-
-    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    Mono<Bag> bag(String id) {
+    @RequestMapping(
+            path = "/{id}",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    Mono<Bag> bag(@PathVariable String id) {
         return bagService.find(id);
     }
 
-    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+
+    @RequestMapping(
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
     Flux<Bag> bags() {
         return bagService.findAll();
     }
 
-    @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
-    Mono<Void> add(BagData bag) {
+
+    @RequestMapping(
+            path = "/add",
+            method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_JSON_VALUE
+    )
+    Mono<Void> add(@RequestBody BagData bag) {
         return bagService.create(toEntity(bag)).then();
     }
 
 
-    @RequestMapping(method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
-    Mono<Void> update(BagData bag) {
-        return bagService.update(toEntity(bag)).then();
+    @RequestMapping(
+            path = "/{id}",
+            method = RequestMethod.PUT,
+            consumes = MediaType.APPLICATION_JSON_VALUE
+    )
+    Mono<Void> update(@PathVariable("id") String id, @RequestBody BagData bag) {
+        return Mono.defer(() -> Mono.just(toEntity(bag)))
+                .doOnNext(b -> b.setId(id))
+                .flatMap(bagService::update)
+                .then();
     }
 }
