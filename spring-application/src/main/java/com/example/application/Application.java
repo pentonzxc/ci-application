d4938b52a7dea42ec17d1fda4286fadb9882b7fa
Nikolai Urusov <nikolai.urusov@gmail.com>
2023-06-25 21:36:26 +0300

    some refactoring...(disable mongo configuration for app, idk if it's working :) )

--- spring-application/src/main/java/com/example/application/Application.java
+++ spring-application/src/main/java/com/example/application/Application.java
@@ -2,16 +2,21 @@ package com.example.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = {
+        MongoAutoConfiguration.class,
+        MongoDataAutoConfiguration.class
+})
 @EnableConfigurationProperties
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
 
 
 }
